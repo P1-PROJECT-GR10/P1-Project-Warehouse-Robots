f5513c14cd886e8d923a5161dc13e217cfa6b0ba
vdalg <victordalg@gmail.com>
2025-11-19 12:32:32 +0100

    Added shelf & item structs, and initial generation helper functions

--- src/warehouse.h
+++ src/warehouse.h
@@ -1,15 +1,19 @@
+// Global vars
 #define MAIN_AISLE_WIDTH 2
 #define AISLE_WIDTH 1
 #define SHELF_LENGTH 6
 #define SHELF_AMOUNT 3
 
+// Global cells
 typedef enum cell {empty, shelf} cell_e;
 
+// Functions
 int* generate_layout(int main_aisle_width, int aisle_width, int shelf_length, int rows, int columns);
 void print_cell(cell_e cell);
 void print_warehouse(int* warehouse, int rows, int columns);
 int* get_cell(int* warehouse, int columns, int x, int y);
 
+// Types
 typedef struct item {
     char name[20];
     double weight;
