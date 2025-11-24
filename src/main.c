b1aa894740e94d11e350d13039f6028320860267
RubenOsloRasmussen <ruben@oslonet.dk>
2025-11-24 10:11:59 +0100

    Fixed bug with movement for robot. Also cleaned up Robot.c somewhat.

--- src/main.c
+++ src/main.c
@@ -14,6 +14,19 @@ int main(void) {
     const int rows = SHELF_AMOUNT * (2 + AISLE_WIDTH) + AISLE_WIDTH;
     const int columns = (MAIN_AISLE_WIDTH * 3 + SHELF_LENGTH * 2);
 
+
+
+
+
+
+
+
+
+
+
+
+
+    
     int* warehouse = generate_layout(MAIN_AISLE_WIDTH, AISLE_WIDTH, SHELF_LENGTH, SHELF_AMOUNT, rows, columns);
 
     robot_t* robot1 = create_robot();
