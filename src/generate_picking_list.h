112ca5a14f7d62aa6649726d116b9aa4d75cbf13
Magnus T <mtronh25@student.aau.dk>
2025-11-26 14:53:27 +0100

    Fixed CMake implementation of new libraries
    Code looks good, could use documentation

--- src/generate_picking_list.h
+++ src/generate_picking_list.h
@@ -1,9 +1,6 @@
-#ifndef P1_PROJECT_GENERATE_PICKING_LIST_H
-#define P1_PROJECT_GENERATE_PICKING_LIST_H
+#pragma once
 
 #include "warehouse.h"
 
 void generate_picking_list(item_t pickingItems[], item_t* items, int item_amount_input, int seed, int n_items);
 void display_picking_list(item_t* pickingItems, int item_amount_input);
-
-#endif //P1_PROJECT_GENERATE_PICKING_LIST_H
